be43e678c52aa30739b9f79b6214ab49e6dc4e5d
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2023-07-06 11:58:09 +0900

    Update docusaurus.config.js

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -32,30 +32,20 @@ const config = {
     locales: ['en'],
   },
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
+  presets: [ 
+    [ 
+      'classic', 
+      /** @type {import('@docusaurus/preset-classic').Options} */ 
+      ({ 
+        docs: { 
+          sidebarPath: require.resolve('./sidebars.js'), 
+          remarkPlugins: [require("remark-breaks")], 
+        }, 
+        theme: { 
+          customCss: require.resolve('./src/css/custom.css'), 
+        }, 
+      }), 
+    ], 
   ],
 
   themeConfig:

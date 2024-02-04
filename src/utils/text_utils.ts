750630d0834f193d9f5c65dbbd3d0b3fae3bb52d
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2024-02-04 16:10:58 +0900

    added TextUtils.replaceRuby()

--- src/utils/text_utils.ts
+++ src/utils/text_utils.ts
@@ -1,6 +1,13 @@
 class TextUtils {
+
+    static readonly rubyRegex = /{RUBY_B#(.+?)}(.+?){RUBY_E#}/g;
+
     static removeRuby(text: string): string {
-        return text.replace(/{RUBY_B#.+?}(.+?){RUBY_E#}/g, "$1");
+        // same as this.replaceRuby(text, (match, base, ruby) => base);
+        return text.replace(this.rubyRegex, "$2");
+    }
+    static replaceRuby(text: string, replacer: (match: string, base: string, ruby: string) => string): string {
+        return text.replace(this.rubyRegex, (match, base, ruby) => replacer(match, base, ruby));
     }
 }
 
